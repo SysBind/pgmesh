ada7a5497ad5bdebdfdce39ef595f2e0dcf8512a
Asaf Ohayon <asaf@sysbind.co.il>
2021-03-25 01:05:52 +0200

    e2e/moodle: wait for postgres to be ready, register admin

--- e2e/moodle/run.sh
+++ e2e/moodle/run.sh
@@ -22,6 +22,11 @@ PG_ENV=" -e POSTGRES_PASSWORD=q1w2e3r4 -e POSTGRES_DB=moodle"
 stdout "running postgres $FROM_VERSION"
 $DOCKER_RUN --name pgmesh-test-pg${FROM_VERSION} ${PG_ENV} postgres:${FROM_VERSION} || fail "failed running postgres:$FROM_VERSION"
 
+while ! docker logs pgmesh-test-pg${FROM_VERSION} | grep "database system is ready to accept connections"; do    
+    stdout "waitin for postgres"
+    sleep 2s
+done
+
 # Moodle
 stdout "building moodle test image"
 docker build --quiet . -f Dockerfile -t pgmesh-test-moodle || fail "failed building moodle test image"
@@ -31,3 +36,8 @@ $DOCKER_RUN -p 80:80 --name pgmesh-test-moodle pgmesh-test-moodle  2>&1 > /dev/n
 
 stdout "running moodle database install"
 $DOCKER_EXC pgmesh-test-moodle php admin/cli/install_database.php --agree-license --adminpass=q1w2e3r4
+
+stdout "completing admin registration"
+$DOCKER_EXC pgmesh-test-moodle psql -e "UPDATE mdl_user SET firstname = 'Admin',lastname = 'User',email = 'test@user.com',maildisplay = '1',country = 'IL',timezone = '99' WHERE id=2"
+$DOCKER_EXC pgmesh-test-moodle psql -e "INSERT INTO mdl_user_preferences (name,value,userid) VALUES('email_bounce_count', '1',2)"
+$DOCKER_EXC pgmesh-test-moodle psql -e "INSERT INTO mdl_user_preferences (name,value,userid) VALUES('email_send_count', '1', 2)"
