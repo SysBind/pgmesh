c02c9d1535a8e6616c325a9d7a992cbf8cfb2ebb
Asaf Ohayon <asaf@sysbind.co.il>
2021-03-31 12:36:47 +0300

    util execution: stdout, stderr via channels to simplify interface

--- cmd/pubsub.go
+++ cmd/pubsub.go
@@ -1,11 +1,8 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
-	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/sysbind/pgmesh/postgres"
@@ -22,35 +19,18 @@ var pubsubCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Starting PubSub")
 
-		dump := pgutil.DumpGlobals(postgres.ConnConfig{
+		stdout, stderr, _ := pgutil.DumpGlobals(postgres.ConnConfig{
 			Host:     "localhost",
 			Database: "moodle",
 			User:     "postgres",
 			Pass:     "q1w2e3r4"})
 
-		stdout, _ := dump.StdoutPipe()
-		stderr, _ := dump.StderrPipe()
-		err := dump.Start()
-		if err != nil {
-			log.Fatal(err)
+		for line := range stdout {
+			fmt.Println(line)
 		}
 
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text()) // Println will add back the final '\n'
-		}
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		}
-
-		stderr_str, err := io.ReadAll(stderr)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if err := dump.Wait(); err != nil {
-			fmt.Printf("%s", stderr_str)
-			log.Fatal(err)
+		for line := range stderr {
+			log.Fatal(line)
 		}
 	},
 }
--- postgres/util/dump.go
+++ postgres/util/dump.go
@@ -1,17 +1,64 @@
 package util
 
 import (
+	"bufio"
+	"io"
 	"os/exec"
 
 	"github.com/sysbind/pgmesh/postgres"
 )
 
-func Dump(conn postgres.ConnConfig) (cmd *exec.Cmd) {
-	cmd = exec.Command("pg_dump", "-U", conn.User, conn.Database)
-	return
+func Dump(conn postgres.ConnConfig) (<-chan string, <-chan string, error) {
+	outchan := make(chan string)
+	errchan := make(chan string)
+
+	cmd := exec.Command("pg_dump", "-U", conn.User, conn.Database)
+
+	err := run(cmd, outchan, errchan)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return outchan, errchan, nil
 }
 
-func DumpGlobals(conn postgres.ConnConfig) (cmd *exec.Cmd) {
-	cmd = exec.Command("pg_dumpall", "--globals-only", "-U", conn.User)
+func DumpGlobals(conn postgres.ConnConfig) (<-chan string, <-chan string, error) {
+	outchan := make(chan string)
+	errchan := make(chan string)
+
+	cmd := exec.Command("pg_dumpall", "--globals-only", "-U", conn.User)
+
+	err := run(cmd, outchan, errchan)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return outchan, errchan, nil
+}
+
+func run(cmd *exec.Cmd, outchan chan<- string, errchan chan<- string) (err error) {
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			outchan <- scanner.Text()
+		}
+		close(outchan)
+	}()
+
+	go func() {
+		stderr_str, _ := io.ReadAll(stderr)
+		errchan <- string(stderr_str)
+		close(errchan)
+		cmd.Wait()
+	}()
+
 	return
 }
